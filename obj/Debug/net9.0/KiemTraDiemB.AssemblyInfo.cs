02112ad4a9c2803dde4a1ad0e2d45feb04525198
“nguyenduydat26” <nguyenduydat26072003@gmail.com>
2025-05-23 10:30:58 +0700

    commit

--- obj/Debug/net9.0/KiemTraDiemB.AssemblyInfo.cs
+++ obj/Debug/net9.0/KiemTraDiemB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KiemTraDiemB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a888ae6b6fe7dbfd349104d6d87c27dd07c817")]
 [assembly: System.Reflection.AssemblyProductAttribute("KiemTraDiemB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KiemTraDiemB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
